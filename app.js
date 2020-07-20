40b0ce5fb1cede8855a14078e80072b7d31035cb
sunil jsk <sunil>
2020-07-20 11:34:54 +0530

    west

---
+++ app.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+  x="hello";
+  angular.module('myfirstApp',[])
+.controller('myfirstcontoller',function ($scope){
+  $scope.name ="sunil";
+  $scope.sayname
+
+});
+
+
+})():
